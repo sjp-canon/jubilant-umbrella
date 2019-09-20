3bcc2fe8b423fd90742a99d38b0485ac812ed12c
Seth Jacobson <sethj11@live.com>
2019-09-19 22:11:21 -0400

    Sets up solution.
    Some files, especially Umbrella.ruleset and the various directory.build.props files are not complete; these will evolve in their content over time.

---
+++ source/Umbrella.Native.Framework/IEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------------------------------------
+// <copyright file="IEnumerableExtensions.cs" company="Canon Games Studio">
+// © 2019, Canon Games Studio. All rights reserved.
+// Licensed under the MIT license. See LICENSE in the project root for full license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------
+
+namespace Umbrella.Native.Framework
+{
+    using System.Collections;
+
+    /// <summary>
+    /// Common extensions for an object implementing <see cref="IEnumerable"/>.
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+    }
+}
